95a9d9ac2bcb1ea223df7262927991c39887ab19
Chen Yi-Cheng <art787472@gmail.com>
2022-08-31 15:01:22 +0800

    feat: set up view engine

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+const exphbs = require('express-handlebars')
 const routes = require('./routes/index')
 
 const app = express()
+
+// set up view engine
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 const port = process.env.PORT || 3000
 
