e725d6a1a931562247c212175f09baf980b08149
Chen Yi-Cheng <art787472@gmail.com>
2022-08-31 14:49:42 +0800

    feat: set up basic routes

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const routes = require('./routes/index')
 
 const app = express()
 
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.end('expense tracker'))
+app.use(routes)
 
 app.listen(port, () => {console.log(`app is on localhost:${port}`)})
